88130b0ad345e8cb473f6dcb16325e6f08422aef
wordtinker <wordtinker@yandex.ru>
2019-01-22 20:22:48 +0300

    struct and impl

---
+++ struct_rect/src/main.rs
@@ -0,0 +1,33 @@
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32
+}
+
+impl Rectangle {
+    // method
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+    // associated function i.e. static method
+    fn square(size: u32) -> Rectangle {
+        Rectangle{width: size, height: size}
+    }
+}
+
+fn main() {
+    let rect1 = Rectangle {width: 30, height: 50};
+    let rect2 = Rectangle {width: 10, height: 40};
+    let rect3 = Rectangle::square(30);
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        rect1.area()
+   );
+   println!("rect1 is {:?}", rect1);
+   println!("rect1 is {:?}", rect2);
+   println!("rect3 is {:?}", rect3);
+   println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+}
