a553f100409928e23e4e5731689a99e35f6f1296
wordtinker <wordtinker@yandex.ru>
2019-01-24 21:43:11 +0300

    lifetime

---
+++ lifetime/src/main.rs
@@ -0,0 +1,32 @@
+fn main() {
+    let string1 = String::from("abcd");
+    let string2 = "xyz";
+
+    let result = longest(string1.as_str(), string2);
+    println!("The longest string is {}", result);
+}
+
+// lifetime annotation
+// result will live as long as both arguments
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
+// won't even compile
+// fn broken() {
+//     let string1 = String::from("long string is long");
+//     let result;
+//     {
+//         let string2 = String::from("xyz");
+//         result = longest(string1.as_str(), string2.as_str());
+//    }
+//    println!("The longest string is {}", result);
+//}
+
+// struct can depend on lifetime too
+struct ImportantExcerpt<'a> {
+    part: &'a str,
+}
