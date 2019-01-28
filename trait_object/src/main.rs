64967486f7982585f45971d521d1b3c3b2f274c6
wordtinker <wordtinker@yandex.ru>
2019-01-28 19:52:26 +0300

    Trait_object

---
+++ trait_object/src/main.rs
@@ -0,0 +1,60 @@
+pub trait Draw {
+    fn draw(&self);
+}
+// NB!
+pub struct Screen {
+    pub components: Vec<Box<dyn Draw>>
+}
+
+impl Screen {
+    pub fn run(&self) {
+        for component in &self.components {
+            component.draw();
+        }
+    }
+}
+
+pub struct Button {
+    pub width: u32,
+    pub height: u32,
+    pub label: String,
+}
+
+impl Draw for Button {
+    fn draw(&self) { /* Do something */ }
+}
+
+pub struct SelectBox {
+    pub width: u32,
+    pub height: u32,
+    pub options: Vec<String>,
+}
+
+impl Draw for SelectBox {
+    fn draw(&self) {
+        // code to actually draw a select box
+    }
+}
+
+fn main() {
+    let screen = Screen {
+        components: vec![
+            Box::new(SelectBox {
+                width: 75,
+                height: 10,
+                options: vec![
+                    String::from("Yes"),
+                    String::from("Maybe"),
+                    String::from("No")
+                ],
+            }),
+            Box::new(Button {
+                width: 50,
+                height: 10,
+                label: String::from("OK"),
+            }),
+        ],
+    };
+
+    screen.run();
+}
